f1504d140644a7afa3c18221a8e817b1b0653ec6
Jinzhu <wosmvp@gmail.com>
2011-11-20 22:19:10 +0800

    update README

--- lib/bitpay.rb
+++ lib/bitpay.rb
@@ -5,8 +5,39 @@ require "bitpay/engine"
 require 'uri'
 
 class Bitpay
-  # buy
-  # return_check
-  # pay_check
-  # refund
+  attr_accessor :options, :name
+
+  def initialize(name=nil, opt=nil)
+    self.name, self.options = name, opt
+  end
+
+  class Checkout < Bitpay
+    def initialize(name=nil, opt=nil)
+      super
+    end
+  end
+
+  class Return < Bitpay
+    def initialize(name=nil, opt=nil)
+      super
+    end
+  end
+
+  class Notification < Bitpay
+    def initialize(name=nil, opt=nil)
+      super
+    end
+  end
+
+  class Verify < Bitpay
+    def initialize(name=nil, opt=nil)
+      super
+    end
+  end
+
+  class Refund < Bitpay
+    def initialize(name=nil, opt=nil)
+      super
+    end
+  end
 end
